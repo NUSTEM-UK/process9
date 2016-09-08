7310110f5a6f69824bed8a034f6d1351ca6b6349
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2016-09-08 13:51:00 +0100

    Clean-ups
    Minor style & comment tidies.

--- process9.py
+++ process9.py
@@ -1,5 +1,15 @@
-# Uses Tesseract (brew install tesseract, on a Mac)
-# and PyTesseract (sudo pip install pytesseract)
+"""Process images containing Diamond 9 data samples.
+
+Uses Tesseract (on a Mac: brew install tesseract) and PyTesseract
+(sudo pip install pytesseract.)
+
+Proof-of-concept for processing captured images into (eventually) coded CSV
+data sets for onward analysis in SPSS. Part of the research efforts
+supporting and drawing lessons from Think Physics, http://thinkphysics.org.
+
+Jonathan Sanderson, Northumbria University, Newcastle, UK.
+This initial test version: 2016-09-07
+"""
 
 import os
 import glob
@@ -7,8 +17,8 @@ import PIL
 import pytesseract
 
 for infile in glob.glob("*.jpg"):
-  file, ext = os.path.splitext(infile)
-  print file
-  # im = Image.open(infile)
-  print pytesseract.image_to_string(PIL.Image.open(infile))
-  print "###############"
+    file, ext = os.path.splitext(infile)
+    print file
+    # im = Image.open(infile)
+    print pytesseract.image_to_string(PIL.Image.open(infile))
+    print "###############"
